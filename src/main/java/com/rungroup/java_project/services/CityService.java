337b260b49d44eca05ff5a259ccc6d333d6f5ddf
Vuk Djukanovic <81326012+Cewu00@users.noreply.github.com>
2024-01-25 06:52:33 +0100

    changes for the latitude_x and longitude_y model fields.

--- src/main/java/com/rungroup/java_project/services/CityService.java
+++ src/main/java/com/rungroup/java_project/services/CityService.java
@@ -1,9 +1,15 @@
 package com.rungroup.java_project.services;
 
 import com.rungroup.java_project.dto.CityDto;
+import com.rungroup.java_project.models.City;
 
 import java.util.List;
 
+// Used for abstraction and decupling
+
+
 public interface CityService {
     List<CityDto> findAllCities();
+    City saveCity(City city);
+
 }
--- src/main/java/com/rungroup/java_project/services/impl/CityServiceImpl.java
+++ src/main/java/com/rungroup/java_project/services/impl/CityServiceImpl.java
@@ -1,37 +1,47 @@
 package com.rungroup.java_project.services.impl;
 
-
+// Internal Imports
 import com.rungroup.java_project.dto.CityDto;
 import com.rungroup.java_project.models.City;
 import com.rungroup.java_project.repository.CityRepository;
 import com.rungroup.java_project.services.CityService;
+
+// Library Imports
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+// Implementacija CityService Interfejsa
+
 @Service
 public class CityServiceImpl implements CityService {
-    private CityRepository citiesRepository;
+    private CityRepository cityRepository;
 
     @Autowired
     public CityServiceImpl(CityRepository citiesRepository) {
-        this.citiesRepository = citiesRepository;
+        this.cityRepository = citiesRepository;
     }
 
     @Override
     public List<CityDto> findAllCities() {
-        List<City> cities = citiesRepository.findAll();
+        List<City> cities = cityRepository.findAll();
         return cities.stream().map((city) -> mapToCityDto(city)).collect(Collectors.toList());
     }
 
-    private CityDto mapToCityDto(City city){
+    @Override
+    public City saveCity(City city) {
+        return cityRepository.save(city);
+    }
+
+    private CityDto mapToCityDto(City city){  // we are only mapping the things we need for the DTO, not the whole city database/model
         CityDto cityDto = CityDto.builder()
                 .id(city.getId())
                 .name(city.getName())
-                .pinGeoLocation(city.getPinGeoLocation())
                 .description(city.getDescription())
+                .latitude_x(city.getLatitude_x())
+                .longitude_y(city.getLongitude_y())
                 .createdOn(city.getCreatedOn())
                 .updatedOn(city.getUpdatedOn())
                 .build();
