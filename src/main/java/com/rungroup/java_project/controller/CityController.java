82e0e74ef3ab7ce4e54be97707e7ed0b1a5c248b
Vuk Djukanovic <81326012+Cewu00@users.noreply.github.com>
2024-01-25 06:53:46 +0100

    saveCity method mapping and definition

--- src/main/java/com/rungroup/java_project/controller/CityController.java
+++ src/main/java/com/rungroup/java_project/controller/CityController.java
@@ -1,11 +1,14 @@
 package com.rungroup.java_project.controller;
 
 import com.rungroup.java_project.dto.CityDto;
+import com.rungroup.java_project.models.City;
 import com.rungroup.java_project.services.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -24,4 +27,19 @@ public class CityController {
         model.addAttribute("cities", cities);
         return "cities-list";
     }
+    @GetMapping("/cities/new")
+    public String createCityForm(Model model) {
+        City city = new City();
+        model.addAttribute("city", city);
+
+        return "cities-create";
+
+    }
+    @PostMapping("/cities/new")
+    public String saveCity(@ModelAttribute("city") City city) {
+        cityService.saveCity(city);
+        return "redirect:/cities";
+    }
+
+
 }
