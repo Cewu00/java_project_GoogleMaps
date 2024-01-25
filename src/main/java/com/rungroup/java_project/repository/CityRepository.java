2f7dc9d2b29db78160c88cb2bfdeec1f90d59dfe
Vuk Djukanovic <81326012+Cewu00@users.noreply.github.com>
2024-01-25 06:53:01 +0100

    comment for clarity

--- src/main/java/com/rungroup/java_project/repository/CityRepository.java
+++ src/main/java/com/rungroup/java_project/repository/CityRepository.java
@@ -7,5 +7,8 @@ import java.util.Optional;
 
 public interface CityRepository extends JpaRepository<City, Long> {
     Optional<City> findByName(String url);
-
+    // findByName is a CUSTOM QUERY METHOD,
+    // java sees "findBy" and "Name" and then finds the "Name" property in the <City> model
+    // then it just creates the sql code necessary for the extraction of all cities by name.
+    // that is how I understood it
 }
