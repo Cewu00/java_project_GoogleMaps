567946a2e566204c93e36c94a4136d2824abf882
Vuk Djukanovic <81326012+Cewu00@users.noreply.github.com>
2024-02-05 11:51:33 +0100

    dodao redirect sa pocetne stranice kako ne bi morao da pises /cities svaki put

---
+++ src/main/java/com/rungroup/java_project/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.rungroup.java_project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "redirect:/cities";
+    }
+
+}
