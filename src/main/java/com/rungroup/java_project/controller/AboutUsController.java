1b009be943f60bfc05e0b000422f965e00cd189f
Vuk Djukanovic <81326012+Cewu00@users.noreply.github.com>
2024-02-05 21:49:30 +0100

    makao import koji se ne koristi

--- src/main/java/com/rungroup/java_project/controller/AboutUsController.java
+++ src/main/java/com/rungroup/java_project/controller/AboutUsController.java
@@ -2,7 +2,6 @@ package com.rungroup.java_project.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
